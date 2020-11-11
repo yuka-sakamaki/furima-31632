66e57638e6a8f4b2931267120008b6f6785879d3
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-11-11 10:57:08 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root to: "items#index"
+=======
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+>>>>>>> parent of 44bbd26... Revert "deviseの導入"
 end
