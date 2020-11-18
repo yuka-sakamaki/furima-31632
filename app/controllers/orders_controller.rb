4700224bddbbe6a263dca9ee41f5891b34653687
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-11-18 20:25:58 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,10 +42,8 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id == @item.user_id
-      redirect_to root_path
-    else @item.order.present?
-      redirect_to root_path
-    end
+     if current_user.id == @item.user_id
+        redirect_to root_path
+     end
   end
 end
