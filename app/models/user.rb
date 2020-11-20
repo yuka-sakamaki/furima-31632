c5270b8d7d871e72f8771b2de508110c1ecea606
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-11-20 09:13:06 +0900

    本番環境の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
   validates :nickname, :last_name, :first_name, :last_name_katakana, :first_name_katakana, :birthday, presence: true
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name_katakana, :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :password, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
